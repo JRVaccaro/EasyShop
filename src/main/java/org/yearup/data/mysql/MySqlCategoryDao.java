103b28e18d65de3c69c164039c459f7f8b4a31a2
JRVaccaro <ooliebud@gmail.com>
2025-06-23 14:25:58 -0400

    Implemented getAllCategories method in MySqlCategoryDao to get all categories from database.

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -5,9 +5,15 @@ import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+
+import static org.yearup.data.mysql.MySqlProductDao.mapRow;
 
 @Component
 public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
@@ -20,8 +26,26 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
     @Override
     public List<Category> getAllCategories()
     {
-        // get all categories
-        return null;
+        List<Category> categories = new ArrayList<>();
+
+        String sql = "SELECT * FROM categories";
+
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet row = statement.executeQuery();
+
+                while (row.next())
+                {
+                    Category category = mapRow(row);
+                categories.add(category);
+
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+        return categories;
     }
 
     @Override
@@ -57,11 +81,13 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
         String description = row.getString("description");
 
         Category category = new Category()
-        {{
+        {
+            {
             setCategoryId(categoryId);
             setName(name);
             setDescription(description);
-        }};
+        }
+        };
 
         return category;
     }
