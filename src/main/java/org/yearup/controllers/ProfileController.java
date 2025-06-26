aaa91e9ee488d297cb7c88a67ac1202f8ed5b301
JRVaccaro <ooliebud@gmail.com>
2025-06-26 14:46:48 -0400

    Added PUT endpoint to UPDATE user profile.

--- src/main/java/org/yearup/controllers/ProfileController.java
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -2,10 +2,7 @@ package org.yearup.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProfileDao;
 import org.yearup.models.Profile;
@@ -42,6 +39,27 @@ public class ProfileController
                 throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User does not exist");
 
             return profileDao.getByUserId(user.getId());
+        }
+        catch(Exception e)
+        {
+            //If any other errors occur, return a 500 error
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
+    }
+    @PutMapping("")
+    public void updateProfile(@RequestBody Profile profile, Principal principal){
+        try {
+            // get the currently logged in username from the principal object
+            String userName = principal.getName();
+
+            // Use username to find the user details in database
+            User user = userDao.getByUserName(userName);
+
+            //If user is not found, return a 404 error
+            if (user == null)
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User does not exist");
+
+            profileDao.update(user.getId(), profile);
         }
         catch(Exception e)
         {
