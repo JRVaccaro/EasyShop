8f4352d6d1e2551d9096bf8fc874ca6d85e8d72e
JRVaccaro <ooliebud@gmail.com>
2025-06-25 23:03:32 -0400

    Added deleteInCart method signature to ShoppingCartDao interface.

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -4,9 +4,15 @@ import org.yearup.models.ShoppingCart;
 
 public interface ShoppingCartDao
 {
+    //Gets the shopping cart and it's items from logged in user by userId
     ShoppingCart getByUserId(int userId);
-    // add additional method signatures here
 
+//Add products to cart
 void addToCart (int userId, int productId);
+
+//Update quantity in cart
 void updateCart (int userId, int productId);
+
+//Remove items from cart
+void deleteInCart(int userId);
 }
