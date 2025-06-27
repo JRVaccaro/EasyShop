d795c7c9f5cf31be2c2d63e1c890c3c4ef40a1e8
JRVaccaro <ooliebud@gmail.com>
2025-06-27 00:11:00 -0400

    Fully implemented MySqlOrderLineItemDao with create method complete.

--- src/main/java/org/yearup/data/mysql/MySqlOrderLineItemDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlOrderLineItemDao.java
@@ -6,6 +6,10 @@ import org.yearup.models.Order;
 import org.yearup.models.OrderLineItem;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Component
 public class MySqlOrderLineItemDao extends MySqlDaoBase implements OrderLineItemDao {
@@ -16,7 +20,37 @@ public class MySqlOrderLineItemDao extends MySqlDaoBase implements OrderLineItem
     }
     @Override
     public OrderLineItem create(OrderLineItem orderLineItem){
+        //Statement to insert a new order line item
         String sql = "INSERT INTO order_line_items (order_id, product_id, sales_price, quantity, discount) " +
-             "VALUES (?, ?, ?, ?, ?";
+             "VALUES (?, ?, ?, ?, ?)";
+
+        try (Connection connection = getConnection()) {
+            // Prepare statement with option to retrieve generated key
+            PreparedStatement statement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+
+            statement.setInt(1, orderLineItem.getOrderId());
+            statement.setInt(2, orderLineItem.getProductId());
+            statement.setBigDecimal(3, orderLineItem.getSalesPrice());
+            statement.setInt(4, orderLineItem.getQuantity());
+            statement.setBigDecimal(5, orderLineItem.getDiscount());
+
+            int rowsAffected = statement.executeUpdate();
+
+            //If insert succeeded, get the generated ID
+            if (rowsAffected > 0) {
+                ResultSet generatedKeys = statement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    int newId = generatedKeys.getInt(1);
+                    //Set generated ID to orderLineItem object
+                    orderLineItem.setOrderLineItemId(newId);
+                    return orderLineItem;
+                }
+            }
+
+    } catch (SQLException e) {
+        throw new RuntimeException(e);
     }
+
+        return null;
+}
 }
