df1e493611ef81fe03ff551099d20d3085e40695
JRVaccaro <ooliebud@gmail.com>
2025-06-26 21:55:04 -0400

    Create Order model to match orders table in database.

---
+++ src/main/java/org/yearup/models/Order.java
@@ -0,0 +1,15 @@
+package org.yearup.models;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Order {
+    private int orderId;
+    private int userId;
+    private LocalDateTime date;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private BigDecimal shippingAmount;
+}
