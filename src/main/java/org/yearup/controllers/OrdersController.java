e4da0889ba96c6a57ba808fa8dd90431acadad4b
JRVaccaro <ooliebud@gmail.com>
2025-06-27 02:05:19 -0400

    Fixed OrdersController to use userDao instance for getting user by username.

--- src/main/java/org/yearup/controllers/OrdersController.java
+++ src/main/java/org/yearup/controllers/OrdersController.java
@@ -1,8 +1,15 @@
 package org.yearup.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.OrderDao;
+import org.yearup.data.ShoppingCartDao;
 import org.yearup.models.Order;
+import org.yearup.models.User;
+import org.yearup.data.UserDao;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/orders")
@@ -10,12 +17,34 @@ import org.yearup.models.Order;
 
 public class OrdersController {
     private OrderDao orderDao;
+    private ShoppingCartDao shoppingCartDao;
+    private UserDao userDao;
 
-    public OrdersController(OrderDao orderDao) {
+    public OrdersController(OrderDao orderDao, ShoppingCartDao shoppingCartDao, UserDao userDao) {
         this.orderDao = orderDao;
+        this.shoppingCartDao = shoppingCartDao;
+        this.userDao = userDao;
     }
+
     @PostMapping
-    public Order createOrder(@RequestBody Order order){
-        return orderDao.create(order);
+    @ResponseStatus(HttpStatus.CREATED)
+    public Order createOrder(@RequestBody Order order, Principal principal){
+
+        String userName = principal.getName();
+        User user = userDao.getByUserName(userName);
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User does not exist");
+        }
+        //Set userId on order
+        order.setUserId(user.getId());
+
+        //Create the order
+        Order createdOrder = orderDao.create(order);
+
+        //Clear the user's shopping cart after order is created
+        shoppingCartDao.clearCart(user.getId());
+
+        return createdOrder;
+
     }
 }
