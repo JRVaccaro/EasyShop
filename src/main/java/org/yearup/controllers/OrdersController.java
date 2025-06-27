67507daa6827c130117622895144bb2dd7748d1e
JRVaccaro <ooliebud@gmail.com>
2025-06-27 00:24:24 -0400

    Added createOrder endpoint for OrdersController class.

--- src/main/java/org/yearup/controllers/OrdersController.java
+++ src/main/java/org/yearup/controllers/OrdersController.java
@@ -1,12 +1,21 @@
 package org.yearup.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.yearup.data.OrderDao;
+import org.yearup.models.Order;
 
 @RestController
-@RequestMapping
+@RequestMapping("/orders")
 @CrossOrigin
 
 public class OrdersController {
+    private OrderDao orderDao;
+
+    public OrdersController(OrderDao orderDao) {
+        this.orderDao = orderDao;
+    }
+    @PostMapping
+    public Order createOrder(@RequestBody Order order){
+        return orderDao.create(order);
+    }
 }
